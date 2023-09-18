0dd90982830dad42f837257feec2ada68492a5fd
Comodaino <alespineto@gmail.com>
2023-09-18 18:18:23 +0200

    little fix

--- sl.c
+++ sl.c
@@ -109,7 +109,7 @@ int main(int argc, char *argv[])
         }
         getch();
         refresh();
-        usleep(4i00);
+        usleep(400);
     }
     mvcur(0, COLS - 1, LINES - 1, 0);
     endwin();
